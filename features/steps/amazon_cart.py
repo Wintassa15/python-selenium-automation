fdc8dbeb0da4e36af4e8c07d9faf5dd941c54de2
Eyerusalem Gebregiorgis <wintana@Eyerusalems-MBP.attlocal.net>
2020-10-18 10:22:55 -0500

    fourth hw

--- features/steps/amazon_cart.py
+++ features/steps/amazon_cart.py
@@ -1,42 +1,45 @@
-from selenium import webdriver
-from behave import Given, When, Then
+
+from behave import given, when, then
 from time import sleep
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome(
-    executable_path="/Users/wintana/JobEasy-QAAutomation/python-selenium-automation/chromedriver-2")
 search = (By.ID, 'twotabsearchtextbox')
 search_icon = (By.CSS_SELECTOR, "input.nav-input[value='Go']")
 select_item = (By.CSS_SELECTOR, "img.s-image[src='https://m.media-amazon.com/images/I/811mZ5F0YVL._AC_UL320_.jpg']")
 add_cart = (By.CSS_SELECTOR, "input#add-to-cart-button")
-number_items = (By.XPATH, "//span[@class='sc-without-fresh-inline']")
+#number_items = (By.XPATH, "//span[@class='sc-without-fresh-inline']")
+number_items=(By.ID, 'sc-subtotal-label-buybox')
+open_cart = (By.CSS_SELECTOR, "nav-cart")
 
 
-@Given('open amazon website')
+@given('open amazon website')
 def open_amazon(context):
     context.driver.get("https://www.amazon.com")
     context.driver.maximize_window()
     sleep(4)
 
 
-@When('input teddy bear into search field')
+@when('input teddy bear into search field')
 def teddy_bear_search(context):
-    search_item = context.driver.find_element(*search).click()
-    context.driver.send_keys("teddy bear")
-    search_icon.click()
+    search_item = context.driver.find_element(*search)
+    search_item .send_keys("teddy bear")
+    context.driver.find_element(*search_icon).click()
+    #search_icon.click()
+    sleep(3)
 
 
-@Then('select an item')
-def select(context):
+@when('click on the product')
+def click_product(context):
     context.driver.find_element(*select_item).click()
 
 
-@Then('add to cart')
+@when('click on add to cart')
 def add_item(context):
     context.driver.find_element(*add_cart).click()
 
 
-@Then('verify number of items')
+@then('verify number of items')
 def verify_number(context):
+    context.driver.find_element(*open_cart).click()
     verify = context.driver.find_element(*number_items)
     assert 'Subtotal (1 item)' in verify.text
--- features/tests/amazon_cart.feature
+++ features/tests/amazon_cart.feature
@@ -5,7 +5,6 @@ Feature:  Test Scenario for items in cart
   Scenario: Verify number of items in cart
     Given open amazon website
     When input teddy bear into search field
-    And click on search icon
-    Then select an item
-    And add to cart
-    And verify number of items
+    And click on the product
+    And click on add to cart
+    Then verify number of items
