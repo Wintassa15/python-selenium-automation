b8ba3a29186866d21e01eba151548b4528fef9d2
Eyerusalem Gebregiorgis <wintana@Eyerusalems-MBP.attlocal.net>
2020-12-18 13:26:17 -0600

    revised fifth HW

--- features/steps/color_selection.py
+++ features/steps/color_selection.py
@@ -12,20 +12,11 @@ def open_amazon_page(context, productid):
 
 @then('verify that user can select colors')
 def user_select_color(context):
-    # expected_colors = ['Black', 'Blue Overdyed', 'Dark Wash', 'Indigo Wash', 'Light Wash', 'Medium Wash', 'Rinse',
-    #                    'Light Wash']
-    #expected_colors = ['Black', 'Blue Overdyed', 'Dark Vintage', 'Dark Wash', 'Indigo Wash', 'Light Vintage', 'Light Wash', 'Medium Vintage', 'Medium Wash', 'Rinse', 'Vintage Light Wash', 'Washed Black', 'Rinsed Vintage']
-    colors = context.driver.find_elements(*COLOR_OPTIONS)
 
-    # for i in range(len(colors)):
-    #     colors(i).click()
-    #     color_text = context.driver.find_element(*SELECTED_COLOR).text
-    #     assert color_text == expected_colors[i], f'colors do not match. Expected {expected_colors[i]} but got ' \
-    #                                                   f'{color_text}'
+    colors = context.driver.find_elements(*COLOR_OPTIONS)
 
     for color in colors:
         color.click()
         assert context.driver.find_element(*SELECTED_COLOR).text != ''
-
 
 
